c19c0300773dffc4bd066abfff0e8625e496670c
thinkGrow <rubaitreshad@gmail.com>
2024-12-21 16:03:32 +0600

    Contact Header Styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import Navigation from "./components/Navigation/Navigation";
 
 import "./App.css";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <ContactHeader />
     </div>
   );
 }
