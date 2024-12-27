40103288558bd0e86ffe7b513a709e335d579b15
thinkGrow <rubaitreshad@gmail.com>
2024-12-27 20:01:27 +0600

    contact-form structure

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -16,11 +16,30 @@ const ContactForm = () => {
           />
           <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
         </div>
+
         <Button
           isOutline={true}
           text="VIA EMAIL FORM"
           icon={<HiMail fontSize="24px" />}
         />
+
+        <form action="">
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="text">Text</label>
+            <textarea name="text" />
+          </div>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <Button text="SUBMIT BUTTON" />
+          </div>
+        </form>
       </div>
       <div className={styles.contact_image}></div>
     </section>
